0426e109ad34b069bba56e5cb08901e4006c8e20
Hyesun Jung <tjs531@naver.com>
2020-09-08 18:19:19 +0900

    Add files via upload

---
+++ MatZip/src/com/koreait/matzip/db/JdbcTemplate.java
@@ -0,0 +1,47 @@
+package com.koreait.matzip.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class JdbcTemplate {
+	
+	//select에 쓸 메소드
+	public static void executeQuery(String sql, JdbcSelectInterface jdbc) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs=null;
+		try {
+			con=DbManager.getCon();
+			ps=con.prepareStatement(sql);
+			jdbc.prepared(ps);
+			rs = ps.executeQuery();
+			jdbc.executeQuery(rs);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DbManager.close(con, ps, rs);
+		}
+	}
+	
+	//insert, update, delete에 쓸 메소드
+	public static int executeUpdate(String sql,JdbcUpdateInterface jdbc) {
+		int result = 0;
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		try {
+			con= DbManager.getCon(); 
+			ps = con.prepareStatement(sql);
+			
+			jdbc.update(ps);
+			result = ps.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DbManager.close(con, ps);
+		}
+		
+		return result;
+	}
+}
