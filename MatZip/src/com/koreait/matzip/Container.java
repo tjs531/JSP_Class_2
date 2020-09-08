08e0202ea0b564f5af52dc04f236e0c6320a8689
Hyesun Jung <tjs531@naver.com>
2020-09-08 18:09:50 +0900

    Update Container.java

--- MatZip/src/com/koreait/matzip/Container.java
+++ MatZip/src/com/koreait/matzip/Container.java
@@ -1,6 +1,8 @@
 package com.koreait.matzip;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,19 +23,29 @@ public class Container extends HttpServlet {
 		proc(request, response);
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		proc(request, response);
 	}
 	
 	private void proc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String temp = mapper.nav(request);
 		
-		if(temp.indexOf("/") >= 0 && "redirect:".equals(temp.substring(0, temp.indexOf("/")))) {
+		if(temp.indexOf(":") >= 0) {
+			String prefix = temp.substring(0, temp.indexOf(":"));
+			String value = temp.substring(temp.indexOf(":")+1 );
 			
-			response.sendRedirect(temp.substring(temp.indexOf("/")));
-			return;
+			if("redirect".equals(prefix)) {
+				response.sendRedirect(value);
+				return;
 			
+			} else if("ajax".equals(prefix)) {
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json");
+				PrintWriter out = response.getWriter();
+				out.print(value);
+				return;
+			}
 		}
 
 		switch(temp) {
